d0a35ed670963d15f41800cdb651d91c52bd92ea
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-01-21 02:01:44 +0800

    Create xxx.js

---
+++ xxx.js
@@ -0,0 +1,14 @@
+var obj = JSON.parse($response.body); 
+// 可以合并一句带过
+obj = {
+  "retcode" : 0,
+  "errmsg" : "",
+  "data" : {
+    "sprows" : [
+
+    ],
+}
+// 有需要全部替换的情况，只需让用户在这里如此操作即可
+
+$done({body:JSON.stringify(obj)});
+// 也一句带过
