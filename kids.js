1dc150eac0ccae0d74056d3a7f53c673b1db61fb
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-03-15 16:57:37 +0800

    Update kids.js

--- kids.js
+++ kids.js
@@ -1,10 +1,7 @@
 var obj = JSON.parse($response.body); 
 // 可以合并一句带过
 
-
-
-
-obj = {"code":200,"type":"","title":"","part":"","player":"","url":"","head":{"user-agent":"Mozilla\/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit\/*(KHTML, like Gecko) *"},"spare":[{"code":200,"success":"","time":"","type":"","mark":"","title":" ","part":"","url":"","head":{"user-agent":"Mozilla\/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit\/* (KHTML, like Gecko) *"}},{"code":200,"url":"","type":"","mark":"","title":"","time":"","head":{"user-agent":"Mozilla\/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit\/*(KHTML, like Gecko) *"}},{"code":200,"success":"","time":"","type":"","title":"","part":,"url":"","head":{"user-agent":"Mozilla\/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit\/*(KHTML, like Gecko) *"}}]}
-
+obj ={
+}
 $done({body:JSON.stringify(obj)});
 // 也一句带过
